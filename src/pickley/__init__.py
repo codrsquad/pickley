37323ac828bc35183ecfdff09bba2e563910ccb5
Zoran Simic <zsimic@netflix.com>
2020-10-26 12:04:20 -0700

    Version 2.1.1

--- src/pickley/__init__.py
+++ src/pickley/__init__.py
@@ -11,7 +11,7 @@ from pickley.env import AvailablePythons, py_version_components, PythonFromPath,
 from pickley.pypi import PepVersion, PypiInfo
 
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 LOG = logging.getLogger(__name__)
 PICKLEY = "pickley"
 VIRTUALENV = "virtualenv"
