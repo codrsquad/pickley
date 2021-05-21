2be8c9162723711502847afd78c6187f6b5f3fe0
Zoran Simic <zsimic@netflix.com>
2021-05-21 15:06:46 -0700

    Downgraded pex

--- src/pickley/package.py
+++ src/pickley/package.py
@@ -296,7 +296,7 @@ class PexPackager(Packager):
         runez.ensure_folder(tmp, logger=False)
         runez.ensure_folder(wheels, logger=False)
         pex_venv = PythonVenv(pspec, folder=os.path.join(build_folder, "pex-venv"))
-        pex_venv.pip_install("pex==2.1.42", *requirements)
+        pex_venv.pip_install("pex==2.1.39", *requirements)
         pex_venv.pip_wheel("--cache-dir", wheels, "--wheel-dir", wheels, *requirements)
         contents = PackageContents(pex_venv, pspec)
         if contents.entry_points:
