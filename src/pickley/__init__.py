584ef5782b8d0e36a09439894908279fa2576a52
Zoran Simic <zsimic@netflix.com>
2023-09-28 13:47:57 -0700

    Version 4.0.0

--- src/pickley/__init__.py
+++ src/pickley/__init__.py
@@ -10,7 +10,7 @@ import runez
 from runez.pyenv import PypiStd, PythonDepot, Version
 
 
-__version__ = "3.5.6"
+__version__ = "4.0.0"
 LOG = logging.getLogger(__name__)
 PICKLEY = "pickley"
 DOT_META = ".pk"
